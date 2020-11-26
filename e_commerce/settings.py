b615b16fec0735b298b18362df726dede4d28ce8
WeversonRibeiro <weversonmt@hotmail.com>
2020-11-26 14:03:01 -0300

    insert STATIC_ROOT in src\e_commerce\setting.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_local")
 ]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
